6a27510509a8f06cfc92ea18929a20ab32d5f359
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-06-11 00:35:25 +0200

    close websocket when token got removed

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,8 +1,13 @@
 const Token = require("../../src/util/Token");
 const Cookie = require("../../src/util/Cookie");
+const WebSocket = require("../../src/websocket/index");
 
 async function resolve(data, apidata) {
     await Token.removeToken(apidata.session.tid);
+
+    const connection = WebSocket.findConnection(apidata.session.id, apidata.session.tid);
+    if (connection) connection.close();
+
     return {
         success: true,
         login: false,
