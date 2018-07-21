5aaa14eb9cfe3f07683819a0be8bb57f20fb259d
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-07-21 14:17:04 +0200

    exporting function directly from api endpoints

--- node/src/api.js
+++ node/src/api.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
     const url = new Url(req.originalUrl, ".js");
 
     if (fs.existsSync(url.fullPath)) {
-        require(this.url.fullPath).resolve(req, res);
+        require(url.fullPath)(req, res);
     } else {
         res.status(404).json({
             success: false,
