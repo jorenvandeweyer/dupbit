7c7eafc2102535f7dcf174f976e56c833261972d
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-11-04 00:11:07 +0100

    destroying tokens successfull

---
+++ node/api/account/destroySessions.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcrypt");
+const db = require("../../src/util/Database");
+const destoryToken = require("../../src/util/destroyToken");
+
+module.exports = async (req, res) => {
+    const data = req.query;
+
+    if (!req.auth.isLoggedIn) return res.errors.needAuth();
+    
+    if (!data.hash) return res.errors.incomplete();
+
+    const hash = await db.getPasswordByID(req.auth.uid);
+    const match = await bcrypt.compare(hash, data.hash);
+    if (!match) return res.errors.wrongCredentials();
+
+    const tokens = await db.getTokens(req.auth.uid);
+    for (token of tokens) {
+        await destoryToken(token.tid, token.uid);
+    }
+
+    res.json({
+        success: "busy",
+    });
+};
