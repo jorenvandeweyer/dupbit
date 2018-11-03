f2882ad6372fd36465c5b39f4fb557daacd4ff28
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2018-11-04 00:19:28 +0100

    updated removeToken, NEEDS TESTING

--- node/api/account/removeToken.js
+++ node/api/account/removeToken.js
@@ -1,32 +1,25 @@
 const db = require("../../src/util/Database");
-const Token = require("../../src/util/Token");
-const ws = require("../../src/websocket/index");
+const destroyToken = require("../../src/util/destroyToken");
 
 module.exports = async (req, res) => {
     const data = req.query;
-    if (req.auth.isLoggedIn && data.tid) {
-        const result = await db.getToken({tid: data.tid});
-        if (result.length) {
-            const token = result[0];
-            if (token.uid === req.auth.uid) {
-                const connection = ws.findConnection(token.uid, token.tid);
-                if (connection) connection.close();
 
-                await Token.removeToken(token.tid);
+    if (!req.auth.isLoggedIn) return res.errors.needAuth();
+    if (!data.tid) return res.errors.incomplete();
 
-                return res.json({
-                    success: true
-                });
-            }
-        }
-        res.json({
-            success: false,
-            reason: "not a token",
-        });
-    } else {
-        res.json({
-            success: false,
-            reason: "invalid params",
-        });
-    }
+    const result = await db.getToken(data.tid);
+
+    if (!result || !result.length) 
+        return res.errors.custom("not a token");
+
+    const token = result[0];
+
+    if (token.uid !== req.auth.uid)
+        return res.errors.custom("not a token");
+
+    await destroyToken(token.tid, token.uid);
+    
+    res.json({
+        success: true,
+    });
 };
