09ac077b52fbdffda7a4eac39877f52ac12e4fe5
Joren Vandeweyer <jorenvandeweyer@gmail.com>
2019-09-28 10:44:59 +0200

    calendar without tests

---
+++ api/src/database/models/calendar.js
@@ -0,0 +1,67 @@
+const Sequelize = require('sequelize');
+
+const Model = Sequelize.Model;
+
+class Calendar extends Model {
+
+}
+
+class CalendarUrl extends Model {
+
+}
+
+class CalendarCourse extends Model {
+
+}
+
+module.exports = (sequelize) => {
+    Calendar.init({
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        }
+    }, {
+        sequelize,
+        modelName: 'calendar',
+    });
+
+    CalendarUrl.init({
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        value: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    }, {
+        sequelize,
+        modelName: 'calendarUrl',
+    });
+
+    CalendarCourse.init({
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        value: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    }, {
+        sequelize,
+        modelName: 'calendarCourse',
+    });
+
+    Calendar.hasMany(CalendarUrl, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    });
+
+    Calendar.hasMany(CalendarCourse, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    });
+
+    return Calendar;
+};
---
+++ tests/api/calls/account/forgot.test.js
@@ -0,0 +1,10 @@
+
+describe('testing account recovery', () => {
+    test('get recovery mail', async () => {
+        expect(true).toBeTruthy();
+    });
+
+    test('validate recovery token', async () => {
+        expect(true).toBeTruthy();
+    });
+});
