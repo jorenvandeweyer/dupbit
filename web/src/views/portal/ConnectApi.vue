450bf777b80aa4b4893392732567d56c02f22c6d
Joren Vandeweyer <joren.vandeweyer@gmail.com>
2020-06-22 11:41:56 +0200

    typo

--- web/src/views/portal/ConnectApi.vue
+++ web/src/views/portal/ConnectApi.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center pt-20 px-12">
-        <h1 lass="text-4xl">Connect API</h1>
+        <h1 class="text-4xl">Connect API</h1>
         <div class="flex flex-row justify-end">
             <div @click="update()" class="bg-white text-black rounded py-1 px-2 mt-4 hover:bg-gray-400 cursor-pointer">Update</div>
         </div>
