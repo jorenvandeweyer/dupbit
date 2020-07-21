d8f51dc523abfaee264e3ad719f8db65dd5f428b
Joren Vandeweyer <joren.vandeweyer@gmail.com>
2020-07-21 17:13:20 +0200

    moved web to its own repo

--- web/src/views/apps/Calendar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="w-full md:w-3/4 lg:w-2/3 pt-20 mx-auto">
-        <h1 class="text-4xl uppercase font-medium">Calendar</h1>
-
-        <div class="text-center mt-8">
-            This project is (temporary) archived :(
-        </div>
-    </div>
-</template>
